b470d93a18d80b52a94c3fc2d32ee2d377369a1d
kbueno1077 <kbueno1077@gmail.com>
2025-01-04 16:56:35 -0500

    Refactor Vercel configuration to version 2, enhancing API route handling with CORS support and defining function memory limits. Transitioned from rewrites to routes for improved serverless function management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless"; // Change this line
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://workinweb.kbueno-studio.com",
   compressHTML: true,
-  output: "hybrid",
-  adapter: vercel(), // This should be using the serverless adapter
+  output: "server",
+  adapter: vercel(),
   routeRules: {
     "/api/**": { prerender: false },
   },
