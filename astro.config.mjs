d85471ac956eeac325ec3e70fd929736b209a8e4
kbueno1077 <kbueno1077@gmail.com>
2025-01-04 16:40:43 -0500

    Update Astro configuration, enhance footer layout, and improve logo component
    - Changed Vercel adapter import to use the serverless version in `astro.config.mjs`.
    - Updated site URL in the configuration to reflect the new domain.
    - Modified the footer layout in `Footer.astro` to include new contact information and a gradient background.
    - Enhanced the `Logo` component to conditionally render a description based on a prop.
    - Improved the structure of the footer with new sections for quick links and services, utilizing icons for contact details.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercelAdapter from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless"; // Change this line
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://workinweb.dev",
+  site: "https://workinweb.kbueno-studio.com",
   compressHTML: true,
   output: "hybrid",
-  adapter: vercelAdapter(),
+  adapter: vercel(), // This should be using the serverless adapter
   routeRules: {
     "/api/**": { prerender: false },
   },
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-export default function Logo() {
+export default function Logo({ withDescriptio = true }) {
   return (
     <a href="/" className="flex items-center space-x-2">
       <div className="text-2xl font-bold">
@@ -6,9 +6,11 @@ export default function Logo() {
         <span className="text-amber-500 dark:text-amber-400">In</span>
         <span className="text-orange-500 dark:text-orange-400">Web</span>
       </div>
-      <div className="hidden sm:block text-sm text-foreground ">
-        // We make it work
-      </div>
+      {withDescriptio && (
+        <div className="hidden sm:block text-sm text-foreground ">
+          // We make it work
+        </div>
+      )}
     </a>
   );
 }
