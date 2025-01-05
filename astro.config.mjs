a21669036e462ba3f4825c9fae8439c0a8be5335
kbueno1077 <kbueno1077@gmail.com>
2025-01-05 01:04:20 -0500

    Update package dependencies: Add @astrojs/netlify and update existing packages in package.json and package-lock.json for improved functionality and compatibility. This change enhances the project structure and ensures alignment with the latest Astro and Vercel features.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+import netlifyIntegration from "@astrojs/netlify";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   compressHTML: true,
   output: "hybrid",
-  adapter: vercel(),
+  adapter: netlifyIntegration(),
 });
