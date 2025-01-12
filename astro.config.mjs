1f09eb062bdaf81f9f027da2b84c48b120a8737e
kbueno1077 <kbueno1077@gmail.com>
2025-01-12 16:05:20 -0500

    Update project configuration and package details: Changed project name from "workinweb-landing" to "workinweb" and updated version to 0.0.2 in package.json. Modified Astro configuration to enable web analytics in the Vercel adapter, enhancing tracking capabilities. These changes improve project organization and analytics integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import netlifyIntegration from "@astrojs/netlify";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   compressHTML: true,
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: true,
+  }),
 });
