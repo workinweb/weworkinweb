5275f064ab9a8dcba482a79c9d9ca2660543ba1c
kbueno1077 <kbueno1077@gmail.com>
2025-01-13 17:55:42 -0500

    Enhance SEO and site configuration: Added site URL to astro.config.mjs and implemented new SEO meta tags in Layout.astro, including robots, author, copyright, theme color, language alternates, canonical URL, and sitemap link. These updates improve site visibility and indexing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  site: "https://weworkinweb.com",
   integrations: [react(), tailwind()],
   compressHTML: true,
   output: "hybrid",
