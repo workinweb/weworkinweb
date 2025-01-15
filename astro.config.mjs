ceb8c2d199e2343e8b72b6a680bf3d69a8315176
kbueno1077 <kbueno1077@gmail.com>
2025-01-15 17:38:13 -0500

    Integrate Partytown for improved performance: Added Partytown integration to the Astro configuration for optimized loading of Google Tag Manager scripts. Updated package dependencies to include @astrojs/partytown and added relevant scripts in Layout and index pages for enhanced client-side performance.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://weworkinweb.com",
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   compressHTML: true,
   output: "hybrid",
   adapter: vercel({
